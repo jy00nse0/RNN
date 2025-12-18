65b88b31d78adf6528063d4098f0bdafb7b8b394
Jang Yoon seo <123075825+jy00nse0@users.noreply.github.com>
2025-12-18 20:46:12 +0900

    Update embeddings.py

--- model/seq2seq/embeddings.py
+++ model/seq2seq/embeddings.py
@@ -1,9 +1,11 @@
 import torch.nn as nn
 
-
 def embeddings_factory(args, metadata):
     embed = nn.Embedding(num_embeddings=metadata.vocab_size, embedding_dim=args.embedding_size,
                          padding_idx=metadata.padding_idx, _weight=metadata.vectors)
-    # whether we will propagate gradients to word embeddings
-    embed.weight.require_grads = args.train_embeddings
+    
+    # [Fixed] Typos fix: require_grads -> requires_grad
+    # This ensures that embedding freezing works as intended (e.g. for pre-trained vectors)
+    embed.weight.requires_grad = args.train_embeddings
+    
     return embed
